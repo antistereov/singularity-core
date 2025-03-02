2d0aa466bc541df50ac3dbb571010a95853456eb
antistereov <admin@stereov.io>
2025-03-02 17:19:28 +0100

    build: bump version to 0.1.5-SNAPSHOT

--- web-spring-boot-autoconfigure/build.gradle.kts
+++ web-spring-boot-autoconfigure/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
 }
 
 group = "io.stereov.web"
-version = "0.1.4"
+version = "0.1.5-SNAPSHOT"
 
 val accessToken: String? = properties["maven.accessToken"] as String? ?: System.getenv("GITEA_TOKEN")
 
