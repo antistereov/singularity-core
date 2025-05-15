4369a38bd7e6bb6c596403eabd2cedd3740acd9f
antistereov <andre.antimonov@stereov.io>
2025-05-15 22:12:30 +0200

    chore(content): remove unused methods and imports in ArticleRepository

--- libs/content/src/main/kotlin/io/stereov/singularity/content/article/repository/ArticleRepository.kt
+++ libs/content/src/main/kotlin/io/stereov/singularity/content/article/repository/ArticleRepository.kt
@@ -2,12 +2,7 @@ package io.stereov.singularity.content.article.repository
 
 import io.stereov.singularity.content.article.model.Article
 import io.stereov.singularity.content.common.repository.ContentRepository
-import kotlinx.coroutines.flow.Flow
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ArticleRepository : ContentRepository<Article> {
-
-    suspend fun findByIdLessThanOrderByIdDesc(id: String, limit: Long): Flow<Article>
-}
+interface ArticleRepository : ContentRepository<Article>
