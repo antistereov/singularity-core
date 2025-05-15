357fe0bc077e32ada846cbf842cc385a9921638b
antistereov <andre.antimonov@stereov.io>
2025-05-15 22:17:31 +0200

    test(content): update save function after changes in article and content API

--- apps/integration-tests/src/test/kotlin/io/stereov/singularity/test/BaseContentTest.kt
+++ apps/integration-tests/src/test/kotlin/io/stereov/singularity/test/BaseContentTest.kt
@@ -1,21 +1,17 @@
 package io.stereov.singularity.test
 
-import io.stereov.singularity.content.article.dto.FullArticleDto
+import io.stereov.singularity.content.article.dto.CreateArticleRequest
+import io.stereov.singularity.content.article.dto.FullArticleResponse
 import io.stereov.singularity.content.article.model.Article
-import io.stereov.singularity.content.article.model.ArticleColors
-import io.stereov.singularity.content.article.model.ArticleContent
-import io.stereov.singularity.content.article.model.ArticleState
 import io.stereov.singularity.content.article.service.ArticleService
-import io.stereov.singularity.core.auth.model.AccessType
 import io.stereov.singularity.core.config.Constants
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.AfterEach
 import org.springframework.beans.factory.annotation.Autowired
-import java.time.Instant
 
 class BaseContentTest : BaseIntegrationTest() {
 
-    val contentBasePath = "$basePath/content"
+    final val contentBasePath = "$basePath/content"
 
     @Autowired
     lateinit var articleService: ArticleService
@@ -25,41 +21,21 @@ class BaseContentTest : BaseIntegrationTest() {
         articleService.deleteAll()
     }
 
-    val fullArticle = FullArticleDto(
-        id = null,
-        key = "article-key",
-        createdAt = Instant.now(),
-        updatedAt = Instant.now(),
-        publishedAt = null,
-        creator = null,
-        path = Article.basePath + "/article-key",
-        state = ArticleState.DRAFT,
-        title = "Title",
-        colors = ArticleColors("black", "white"),
-        summary = "Summary",
-        image = null,
-        content = ArticleContent("content", listOf()),
-        accessType = AccessType.PUBLIC,
-        canView = mutableSetOf(),
-        canEdit = mutableSetOf(),
-    )
-
-    suspend fun save(creator: TestRegisterResponse? = null, article: FullArticleDto = fullArticle): FullArticleDto {
+    suspend fun save(creator: TestRegisterResponse? = null, title: String? = null): Article {
         val actualUser = creator ?: registerUser()
-        val actualArticle = article.copy(creator = actualUser.info.toOverviewDto())
-
-        val res = webTestClient.put()
-            .uri("/api/content/articles")
+        val req = CreateArticleRequest(title ?: "test")
+        val res = webTestClient.post()
+            .uri("/api/content/articles/create")
             .cookie(Constants.ACCESS_TOKEN_COOKIE, actualUser.accessToken)
-            .bodyValue(actualArticle)
+            .bodyValue(req)
             .exchange()
             .expectStatus().isOk
-            .expectBody(FullArticleDto::class.java)
+            .expectBody(FullArticleResponse::class.java)
             .returnResult()
             .responseBody
 
         requireNotNull(res)
 
-        return res
+        return Article.create(res)
     }
 }
