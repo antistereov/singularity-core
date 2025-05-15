4b950d83ff004718ca9dd8381902058c5d6e41a8
antistereov <andre.antimonov@stereov.io>
2025-05-15 22:04:51 +0200

    feat(core): add groups to UserDocument

--- libs/core/src/main/kotlin/io/stereov/singularity/core/user/model/SensitiveUserData.kt
+++ libs/core/src/main/kotlin/io/stereov/singularity/core/user/model/SensitiveUserData.kt
@@ -8,6 +8,7 @@ data class SensitiveUserData(
     var name: String,
     var email: String,
     val roles: MutableSet<Role> = mutableSetOf(Role.USER),
+    val groups: MutableSet<String> = mutableSetOf(),
     val security: UserSecurityDetails = UserSecurityDetails(),
     val devices: MutableList<DeviceInfo> = mutableListOf(),
     var avatar: FileMetaData? = null,
