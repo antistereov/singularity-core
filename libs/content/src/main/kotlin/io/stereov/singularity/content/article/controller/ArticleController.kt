d3fd8541e722639f8caa0658291db41fe3051abd
antistereov <andre.antimonov@stereov.io>
2025-05-16 17:19:12 +0200

    feat(content): create scroll and get requests for articles

--- libs/content/src/main/kotlin/io/stereov/singularity/content/article/controller/ArticleController.kt
+++ libs/content/src/main/kotlin/io/stereov/singularity/content/article/controller/ArticleController.kt
@@ -1,13 +1,12 @@
 package io.stereov.singularity.content.article.controller
 
-import io.stereov.singularity.content.article.dto.ArticleResponse
-import io.stereov.singularity.content.article.dto.ArticleTrustedResponse
-import io.stereov.singularity.content.article.dto.CreateArticleRequest
-import io.stereov.singularity.content.article.dto.FullArticleResponse
+import io.stereov.singularity.content.article.dto.*
 import io.stereov.singularity.content.article.model.Article
 import io.stereov.singularity.content.article.service.ArticleManagementService
 import io.stereov.singularity.content.article.service.ArticleService
 import io.stereov.singularity.content.common.controller.ContentController
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
@@ -34,9 +33,17 @@ class ArticleController(
         )
     }
 
-    @GetMapping
+    @GetMapping("/scroll")
     suspend fun getLatestArticles(@RequestParam limit: Long = 10, @RequestParam after: String? = null): ResponseEntity<ArticleResponse> {
         return ResponseEntity.ok(articleManagementService.getAccessibleArticles(limit, after))
+    }
+
+    @GetMapping
+    suspend fun getArticles(@RequestParam page: Int = 0, @RequestParam size: Int = 10): ResponseEntity<Page<ArticleOverviewResponse>> {
+        val pageable = Pageable.ofSize(size).withPage(page)
+        return ResponseEntity.ok(
+            articleManagementService.findAccessible(pageable).map { it.toOverviewResponse() }
+        )
     }
 
     @PostMapping("/create")
