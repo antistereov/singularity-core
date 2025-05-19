0b42e20547402dc33d72b9faf1dc816476639ed7
antistereov <andre.antimonov@stereov.io>
2025-05-19 17:20:19 +0200

    feat(core): add fallback options for translate method in Translatable interface

--- src/main/kotlin/io/stereov/singularity/core/global/language/model/Translatable.kt
+++ src/main/kotlin/io/stereov/singularity/core/global/language/model/Translatable.kt
@@ -1,12 +1,40 @@
 package io.stereov.singularity.core.global.language.model
 
-import io.stereov.singularity.core.global.language.exception.model.TranslationForLangMissingException
+import io.stereov.singularity.core.global.exception.model.DocumentNotFoundException
 
 interface Translatable<C> {
 
     val translations: Map<Language, C>
+    val primaryLanguage: Language
 
-    fun translate(lang: Language): C {
-        return translations[lang] ?: throw TranslationForLangMissingException(lang)
+    fun translate(lang: Language? = null): Pair<Language, C> {
+        return if (lang != null) {
+            getSpecificTranslation(lang) ?: getDefaultTranslation()
+        } else {
+            getDefaultTranslation()
+        }
+    }
+
+    fun getSpecificTranslation(lang: Language): Pair<Language, C>? {
+        val translation = translations[lang]
+
+        return translation?.let { lang to it }
+    }
+
+    fun getDefaultTranslation(): Pair<Language, C> {
+        val en = translations[Language.EN]
+        if (en != null) return Language.EN to en
+
+        val primaryTranslation = translations[primaryLanguage]
+        if (primaryTranslation != null) return primaryLanguage to primaryTranslation
+
+        val firstLang = translations.keys.firstOrNull()
+        val firstTranslation = translations[firstLang]
+
+        if (firstLang == null || firstTranslation == null) {
+            throw DocumentNotFoundException("The requested item does not contain any translations")
+        }
+
+        return firstLang to firstTranslation
     }
 }
