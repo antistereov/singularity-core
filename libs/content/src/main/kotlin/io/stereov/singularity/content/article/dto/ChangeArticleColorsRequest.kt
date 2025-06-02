38574965f8442f8f466b2e6ca10b69e184ec4d71
antistereov <andre.antimonov@stereov.io>
2025-06-02 17:09:04 +0200

    chore(content): remove unused Dtos

--- libs/content/src/main/kotlin/io/stereov/singularity/content/article/dto/ChangeArticleColorsRequest.kt
+++
@@ -1,7 +0,0 @@
-package io.stereov.singularity.content.article.dto
-
-import io.stereov.singularity.content.article.model.ArticleColors
-
-data class ChangeArticleColorsRequest(
-    val colors: ArticleColors
-)
