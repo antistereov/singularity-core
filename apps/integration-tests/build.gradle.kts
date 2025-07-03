ed3b6f99c76dbaa702ff8e2c1f3b7f1d56557501
antistereov <andre.antimonov@stereov.io>
2025-07-03 14:54:36 +0200

    build: fix build setup for repository - remove root build.gradle.kts

--- apps/integration-tests/build.gradle.kts
+++ apps/integration-tests/build.gradle.kts
@@ -1,4 +1,29 @@
-val accessToken = properties["maven.accessToken"] as String?
+plugins {
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.spring") version "2.0.21"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/bitwarden/sdk-sm")
+        credentials {
+            username = properties["gpr.user"] as String?
+                ?: System.getenv("GPR_USER")
+            password = properties["gpr.key"] as String?
+                ?: System.getenv("GPR_KEY")
+        }
+    }
+}
+
+group = properties["group"] as String
+version = properties["version"] as String
+
+kotlin {
+    jvmToolchain(21)
+}
 
 val kotlinxVersion = "1.10.1"
 val springBootVersion = "3.4.1"
--- apps/stereov-io/build.gradle.kts
+++ apps/stereov-io/build.gradle.kts
@@ -1,4 +1,36 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.spring") version "2.0.21"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/bitwarden/sdk-sm")
+        credentials {
+            username = properties["gpr.user"] as String?
+                ?: System.getenv("GPR_USER")
+            password = properties["gpr.key"] as String?
+                ?: System.getenv("GPR_KEY")
+        }
+    }
+}
+
+group = properties["group"] as String
+version = properties["version"] as String
+
+kotlin {
+    jvmToolchain(21)
+}
+
 dependencies {
     api(project(":libs:core"))
     api(project(":libs:content"))
+}
+
+configurations.all {
+    exclude(group = "ch.qos.logback", module = "logback-classic")
+    exclude(group = "org.apache.logging.log4j", module = "log4j-to-slf4j")
 }
