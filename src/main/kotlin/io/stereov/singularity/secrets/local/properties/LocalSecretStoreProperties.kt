b3872b0499d87eeda332cb733208f9880be7226e
antistereov <andre.antimonov@stereov.io>
2025-07-21 15:46:00 +0200

    fix(secret-store): fix property value in LocalSecretStoreProperties

--- src/main/kotlin/io/stereov/singularity/secrets/local/properties/LocalSecretStoreProperties.kt
+++ src/main/kotlin/io/stereov/singularity/secrets/local/properties/LocalSecretStoreProperties.kt
@@ -3,7 +3,7 @@ package io.stereov.singularity.secrets.local.properties
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties("singularity.secrets.sqlite")
+@ConfigurationProperties("singularity.secrets.local")
 @ConditionalOnProperty(prefix = "singularity.secrets", value = ["store"], havingValue = "local", matchIfMissing = true)
 data class LocalSecretStoreProperties(
     val secretDirectory: String = "./.data/secrets"
