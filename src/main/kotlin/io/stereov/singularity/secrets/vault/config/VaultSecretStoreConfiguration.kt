d17fc41decbde631567b9aeff2c494d99ec2ebf9
antistereov <andre.antimonov@stereov.io>
2025-07-21 15:10:13 +0200

    fix(secret-store): add bean declaration for vault secret store

--- src/main/kotlin/io/stereov/singularity/secrets/vault/config/VaultSecretStoreConfiguration.kt
+++ src/main/kotlin/io/stereov/singularity/secrets/vault/config/VaultSecretStoreConfiguration.kt
@@ -1,15 +1,21 @@
 package io.stereov.singularity.secrets.vault.config
 
 import io.stereov.singularity.global.config.ApplicationConfiguration
+import io.stereov.singularity.global.properties.AppProperties
+import io.stereov.singularity.secrets.core.component.SecretStore
+import io.stereov.singularity.secrets.vault.component.VaultSecretStore
 import io.stereov.singularity.secrets.vault.properties.VaultSecretStoreProperties
 import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.vault.authentication.ClientAuthentication
 import org.springframework.vault.authentication.TokenAuthentication
 import org.springframework.vault.client.VaultEndpoint
 import org.springframework.vault.config.AbstractReactiveVaultConfiguration
+import org.springframework.vault.core.ReactiveVaultOperations
 
 @Configuration
 @AutoConfiguration(
@@ -23,6 +29,11 @@ class VaultSecretStoreConfiguration(
     private val properties: VaultSecretStoreProperties
 ) : AbstractReactiveVaultConfiguration()  {
 
+    @Bean
+    @ConditionalOnMissingBean
+    fun vaultSecretStore(properties: VaultSecretStoreProperties, appProperties: AppProperties, vaultOperations: ReactiveVaultOperations): SecretStore {
+        return VaultSecretStore(properties, appProperties, vaultOperations)
+    }
 
     override fun vaultEndpoint(): VaultEndpoint {
         return VaultEndpoint.create(properties.host, properties.port)
