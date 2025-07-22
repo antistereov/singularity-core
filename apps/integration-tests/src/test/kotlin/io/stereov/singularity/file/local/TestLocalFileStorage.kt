45013e9b39db37bbe4abce828f0765dbb7bd9a2a
antistereov <andre.antimonov@stereov.io>
2025-07-22 11:37:50 +0200

    test(local-file-storage): create integration test for file storage

--- apps/integration-tests/src/test/kotlin/io/stereov/singularity/file/local/TestLocalFileStorage.kt
+++ apps/integration-tests/src/test/kotlin/io/stereov/singularity/file/local/TestLocalFileStorage.kt
@@ -1,17 +1,36 @@
 package io.stereov.singularity.file.local
 
+import io.stereov.singularity.content.file.service.FileMetadataService
 import io.stereov.singularity.file.core.service.FileStorage
+import io.stereov.singularity.file.local.properties.LocalFileStorageProperties
 import io.stereov.singularity.file.local.util.MockFilePart
 import io.stereov.singularity.test.BaseIntegrationTest
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.ClassPathResource
+import java.io.File
+import java.time.temporal.ChronoUnit
 
 class TestLocalFileStorage : BaseIntegrationTest() {
 
     @Autowired
     private lateinit var storage: FileStorage
+
+    @Autowired
+    private lateinit var properties: LocalFileStorageProperties
+
+    @Autowired
+    private lateinit var metadataService: FileMetadataService
+
+    @BeforeEach
+    fun delete() = runBlocking {
+        metadataService.deleteAll()
+    }
 
     @Test
     fun `should upload public file`() = runTest {
@@ -20,7 +39,17 @@ class TestLocalFileStorage : BaseIntegrationTest() {
         val key = "test-image"
 
         val metadata = storage.upload(user.info.id, filePart, key, true)
+        val file = File(properties.publicPath, metadata.key)
 
+        assertTrue(file.exists())
+        val savedMetadata = metadataService.findByKey(metadata.key)
 
+        val metadataWithMillis = metadata.copy(
+            createdAt = metadata.createdAt.truncatedTo(ChronoUnit.MILLIS),
+            updatedAt = metadata.updatedAt.truncatedTo(ChronoUnit.MILLIS)
+        )
+        assertEquals(metadataWithMillis, savedMetadata)
+
+        file.delete()
     }
 }
