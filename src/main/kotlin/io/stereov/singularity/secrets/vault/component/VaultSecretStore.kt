77b808115520477b6cd5a8be6507d84a57762ad0
antistereov <andre.antimonov@stereov.io>
2025-07-24 11:45:25 +0200

    fix(vault-secret-store): fix various bugs in implementation of vault

--- src/main/kotlin/io/stereov/singularity/secrets/vault/component/VaultSecretStore.kt
+++ src/main/kotlin/io/stereov/singularity/secrets/vault/component/VaultSecretStore.kt
@@ -6,6 +6,7 @@ import io.stereov.singularity.secrets.core.exception.model.SecretStoreException
 import io.stereov.singularity.secrets.core.model.Secret
 import io.stereov.singularity.secrets.vault.properties.VaultSecretStoreProperties
 import kotlinx.coroutines.reactive.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 import org.springframework.vault.core.ReactiveVaultOperations
@@ -21,14 +22,14 @@ class VaultSecretStore(
 ) : SecretStore {
 
     private val apiPath: String
-        get() = "/v1/${properties.engine}/data/${appProperties.slug}"
+        get() = "${properties.engine}/data/${appProperties.slug}"
 
     private fun getSecretPath(key: String): String = "$apiPath/$key"
 
     override suspend fun getOrNull(key: String): Secret? {
         val secretPath = getSecretPath(key)
 
-        val data = vaultOperations.read(secretPath).awaitSingle().data
+        val data = vaultOperations.read(secretPath).awaitSingleOrNull()?.data
             ?: return null
 
         return data.toSecret(key)
@@ -41,11 +42,12 @@ class VaultSecretStore(
         note: String
     ): Secret {
         val secret = Secret(UUID.randomUUID(), key, value, Instant.now())
-        vaultOperations.write(getSecretPath(key), secret.toMap()).awaitSingle()
+        vaultOperations.write(getSecretPath(key), mapOf("data" to secret.toMap())).awaitSingle()
 
         return secret
     }
 
+    val dataField = "data"
     val idField = "id"
     val valueField = "value"
     val createdAtField = "createdAt"
@@ -53,12 +55,25 @@ class VaultSecretStore(
 
 
     private fun Map<String, Any?>.toSecret(key: String): Secret {
-        val id = (this[idField] as? UUID)
-            ?: throw SecretStoreException("Cannot read secret with key \"$key\": $idField is not of type UUID")
-        val value = this[valueField] as? String
+        val data = this[dataField] as? Map<*, *>
+            ?: throw SecretStoreException("Cannot read secret with key \"$key\": $dataField does not exist or is corrupt")
+
+        val idString = data[idField] as? String
+            ?: throw SecretStoreException("Cannot read secret with key \"$key\": $idField is not of type String")
+        val value = data[valueField] as? String
             ?: throw SecretStoreException("Cannot read secret with key \"$key\": $valueField is not of type String")
-        val createdAt = this[createdAtField] as? Instant
-            ?: throw SecretStoreException("Cannot read secret with key \"$key\": $createdAtField is not of type Instant")
+        val createdAtString = data[createdAtField] as? String
+            ?: throw SecretStoreException("Cannot read secret with key \"$key\": $createdAtField is not of type String")
+
+        val id = try { UUID.fromString(idString) } catch (e: Exception) {
+            throw SecretStoreException("Cannot read secret with key \"$key\": $idField is not of type UUID", e)
+        }
+
+        val createdAt = try {
+            Instant.parse(createdAtString)
+        } catch (e: Exception) {
+            throw SecretStoreException("Cannot read secret with key \"$key\": $createdAtField is not of type Instant", e)
+        }
 
         return Secret(id, key, value, createdAt)
     }
@@ -66,9 +81,9 @@ class VaultSecretStore(
     private fun Secret.toMap(): Map<String, Any> {
         val map = mutableMapOf<String, Any>()
 
-        map[idField] = this.id
+        map[idField] = this.id.toString()
         map[valueField] = this.value
-        map[createdAtField] = this.createdAt
+        map[createdAtField] = this.createdAt.toString()
         map[keyField] = this.key
 
         return map
