ed23d878041fc90c085f9287ef57e39703c5fc29
antistereov <andre.antimonov@stereov.io>
2025-08-11 23:20:59 +0200

    refactor(auth): move geolocation package to auth

--- src/main/kotlin/io/stereov/singularity/auth/geolocation/exception/handler/GeoLocationExceptionHandler.kt
+++ src/main/kotlin/io/stereov/singularity/auth/geolocation/exception/handler/GeoLocationExceptionHandler.kt
@@ -1,7 +1,7 @@
-package io.stereov.singularity.geolocation.exception.handler
+package io.stereov.singularity.auth.geolocation.exception.handler
 
 import io.stereov.singularity.global.exception.BaseExceptionHandler
-import io.stereov.singularity.geolocation.exception.GeoLocationException
+import io.stereov.singularity.auth.geolocation.exception.GeoLocationException
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.server.ServerWebExchange
