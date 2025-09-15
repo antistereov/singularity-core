29bc0972b89000e4f80696210744fc5b8d5099ce
antistereov <andre.antimonov@stereov.io>
2025-09-15 17:53:42 +0200

    docs(auth): add openapi documentation for email verification and password reset

--- src/main/kotlin/io/stereov/singularity/auth/core/controller/EmailVerificationController.kt
+++ src/main/kotlin/io/stereov/singularity/auth/core/controller/EmailVerificationController.kt
@@ -3,13 +3,21 @@ package io.stereov.singularity.auth.core.controller
 import io.stereov.singularity.auth.core.dto.response.MailCooldownResponse
 import io.stereov.singularity.auth.core.service.EmailVerificationService
 import io.stereov.singularity.content.translate.model.Language
+import io.stereov.singularity.global.model.ErrorResponse
+import io.stereov.singularity.global.model.MailSendResponse
+import io.stereov.singularity.global.model.OpenApiConstants
 import io.stereov.singularity.user.core.dto.response.UserResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/auth/email")
+@RequestMapping("/api/auth/email/verify")
 @Tag(
     name = "Email Verification",
     description = "Operations related to email verification."
@@ -18,14 +26,23 @@ class EmailVerificationController(
     private val emailVerificationService: EmailVerificationService,
 ) {
 
-    /**
-     * Verifies the email address of the user using the provided token.
-     *
-     * @param token The verification token sent to the user's email.
-     *
-     * @return The updated user information.
-     */
-    @PostMapping("/verify")
+    @PostMapping
+    @Operation(
+        summary = "Verify email",
+        description = "Verify the user's email by validating the token.",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Updated user information.",
+                content = [Content(schema = Schema(implementation = UserResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "401",
+                description = "Token is invalid.",
+                content = [Content(schema = Schema(implementation = ErrorResponse::class))]
+            ),
+        ]
+    )
     suspend fun verifyEmail(@RequestParam token: String): ResponseEntity<UserResponse> {
         val authInfo = emailVerificationService.verifyEmail(token)
 
@@ -33,32 +50,69 @@ class EmailVerificationController(
             .body(authInfo)
     }
 
-    /**
-     * Gets the remaining cooldown time for email verification.
-     *
-     * @return The remaining cooldown time in seconds.
-     */
-    @GetMapping("/verify/cooldown")
-    suspend fun getRemainingEmailVerificationCooldown(): ResponseEntity<MailCooldownResponse> {
+    @GetMapping("/cooldown")
+    @Operation(
+        summary = "Get remaining email verification cooldown",
+        description = "Get the remaining time in seconds until you can send another verification request.",
+        security = [
+            SecurityRequirement(OpenApiConstants.ACCESS_TOKEN_HEADER),
+            SecurityRequirement(OpenApiConstants.ACCESS_TOKEN_COOKIE)
+        ],
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "The remaining cooldown.",
+                content = [Content(schema = Schema(implementation = MailCooldownResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "401",
+                description = "Unauthorized.",
+                content = [Content(schema = Schema(implementation = ErrorResponse::class))]
+            ),
+        ]
+    )
+    suspend fun getRemainingCooldown(): ResponseEntity<MailCooldownResponse> {
         val remainingCooldown = emailVerificationService.getRemainingCooldown()
 
         return ResponseEntity.ok().body(remainingCooldown)
     }
 
-    /**
-     * Sends a verification email to the user.
-     *
-     * @return A message indicating the success of the operation.
-     */
-    @PostMapping("/verify/send")
+    @PostMapping("/send")
+    @Operation(
+        summary = "Send a new verification email",
+        description = "Send a new verification email to the user's email. " +
+                "After sending an email, a cooldown is triggered " +
+                "during which no new verification email can be sent.",
+        security = [
+            SecurityRequirement(OpenApiConstants.ACCESS_TOKEN_HEADER),
+            SecurityRequirement(OpenApiConstants.ACCESS_TOKEN_COOKIE)
+        ],
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "The number of seconds the user needs to wait before sending a new email.",
+                content = [Content(schema = Schema(implementation = MailCooldownResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "401",
+                description = "Unauthorized.",
+                content = [Content(schema = Schema(implementation = ErrorResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "429",
+                description = "Failed to send another email. Wait until the cooldown is done.",
+                content = [Content(schema = Schema(implementation = ErrorResponse::class))]
+            ),
+        ]
+    )
     suspend fun sendVerificationEmail(
         @RequestParam lang: Language = Language.EN
-    ): ResponseEntity<Map<String, String>> {
+    ): ResponseEntity<MailSendResponse> {
 
         emailVerificationService.sendEmailVerificationToken(lang)
 
         return ResponseEntity.ok().body(
-            mapOf("message" to "Successfully send verification email")
+            MailSendResponse(emailVerificationService.getRemainingCooldown().remaining)
         )
     }
 
