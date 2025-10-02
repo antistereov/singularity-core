1f36e0349d9ff382cddc5ffd26783a1d1ee4e846
antistereov <andre.antimonov@stereov.io>
2025-10-02 13:44:56 +0200

    feat(files): improve file metadata response

--- src/main/kotlin/io/stereov/singularity/file/core/dto/FileMetadataResponse.kt
+++ src/main/kotlin/io/stereov/singularity/file/core/dto/FileMetadataResponse.kt
@@ -1,19 +1,16 @@
 package io.stereov.singularity.file.core.dto
 
+import io.stereov.singularity.content.core.dto.response.ContentAccessDetailsResponse
 import io.stereov.singularity.content.core.dto.response.ContentResponse
-import io.stereov.singularity.content.core.model.ContentAccessDetails
-import io.stereov.singularity.content.core.model.ContentDocument
 import io.stereov.singularity.file.core.model.FileMetadataDocument
 import org.bson.types.ObjectId
 import java.time.Instant
 
 data class FileMetadataResponse(
-    override val id: ObjectId,
-    override val key: String,
-    override val createdAt: Instant = Instant.now(),
-    override var updatedAt: Instant = Instant.now(),
-    override var access: ContentAccessDetails,
+    val id: ObjectId,
+    val key: String,
+    val createdAt: Instant = Instant.now(),
+    var updatedAt: Instant = Instant.now(),
+    var access: ContentAccessDetailsResponse,
     val renditions: Map<String, FileRenditionResponse>,
-    override var trusted: Boolean,
-    override var tags: MutableSet<String>
-) : ContentDocument<FileMetadataResponse>, ContentResponse<FileMetadataDocument>
+) : ContentResponse<FileMetadataDocument>
