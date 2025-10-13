92993d0cc12abd49b27e9cbbdf66971daf5141a0
antistereov <andre.antimonov@stereov.io>
2025-10-13 13:48:15 +0200

    fix(2fa): fix deserialization with annotations instead of custom deserializer

--- src/main/kotlin/io/stereov/singularity/auth/twofactor/model/TwoFactorMethod.kt
+++ src/main/kotlin/io/stereov/singularity/auth/twofactor/model/TwoFactorMethod.kt
@@ -1,5 +1,7 @@
 package io.stereov.singularity.auth.twofactor.model
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
 import io.stereov.singularity.auth.twofactor.exception.model.InvalidTwoFactorMethodException
 
 enum class TwoFactorMethod(val value: String) {
@@ -7,11 +9,14 @@ enum class TwoFactorMethod(val value: String) {
     TOTP("totp"),
     EMAIL("email");
 
+    @JsonValue
     override fun toString(): String {
         return value
     }
 
     companion object {
+        @JvmStatic
+        @JsonCreator
         fun ofString(input: String): TwoFactorMethod {
             return when (input.lowercase()) {
                 "totp" -> TOTP
