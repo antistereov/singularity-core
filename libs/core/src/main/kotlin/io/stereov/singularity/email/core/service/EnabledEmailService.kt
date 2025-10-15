eb56ad029eca467832f33b6b1a87505daf752ffe
antistereov <andre.antimonov@stereov.io>
2025-10-15 13:59:11 +0200

    feat(email): use application name as personal sender

--- libs/core/src/main/kotlin/io/stereov/singularity/email/core/service/EnabledEmailService.kt
+++ libs/core/src/main/kotlin/io/stereov/singularity/email/core/service/EnabledEmailService.kt
@@ -3,6 +3,7 @@ package io.stereov.singularity.email.core.service
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.stereov.singularity.email.core.properties.EmailProperties
+import io.stereov.singularity.global.properties.AppProperties
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,6 +19,7 @@ class EnabledEmailService(
     private val mailSender: JavaMailSender,
     private val emailProperties: EmailProperties,
     private val emailTemplateService: EmailTemplateService,
+    private val appProperties: AppProperties,
 ) : EmailService {
 
     override val logger: KLogger = KotlinLogging.logger {}
@@ -36,7 +38,7 @@ class EnabledEmailService(
         val template = emailTemplateService.createTemplate(subject, content, locale)
 
         val helper = MimeMessageHelper(message, true, "UTF-8")
-        helper.setFrom(emailProperties.email)
+        helper.setFrom(emailProperties.email, appProperties.name)
         helper.setTo(to)
         helper.setSubject(subject)
         helper.setText(template, true)
